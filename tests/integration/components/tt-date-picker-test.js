4904ad1d9f7926d0b9921a2c9a29120db11724ac
Loz Jackson <lozjackson@talktalk.net>
2016-06-20 22:35:02 +0100

    add date-picker component

---
+++ tests/integration/components/tt-date-picker-test.js
@@ -0,0 +1,15 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('tt-date-picker', 'Integration | Component | tt date picker', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });"
+
+  this.render(hbs`{{tt-date-picker}}`);
+
+  assert.equal(this.$('.tt-date-picker.container').length, 1);
+});
