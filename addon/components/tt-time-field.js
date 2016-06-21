a9f9f145ed03304dc25f162accf5c529c6b11142
Loz Jackson <lozjackson@talktalk.net>
2016-06-21 18:21:19 +0100

    add time-field component

---
+++ addon/components/tt-time-field.js
@@ -0,0 +1,49 @@
+/**
+  @module ember-time-tools
+*/
+import Ember from 'ember';
+
+/**
+  A read-only text field component for displaying a time
+
+  @class TimeFieldComponent
+  @namespace Components
+*/
+export default Ember.TextField.extend({
+
+  /**
+    @property tagName
+    @type {String}
+    @default input
+  */
+  tagName: 'input',
+
+  /**
+    @property classNames
+    @type {Array}
+    @default ['no-select', 'tt-time-field']
+  */
+  classNames: ['no-select', 'tt-time-field'],
+
+  /**
+    @property size
+    @type {Number}
+    @default 8
+  */
+  size: 8,
+
+  /**
+    @event click
+  */
+  click(e) {
+    e.target.blur();
+    this.sendAction('toggleTimePicker');
+  },
+
+  /**
+    @event keyPress
+  */
+  keyPress() {
+    return false;
+  }
+});
