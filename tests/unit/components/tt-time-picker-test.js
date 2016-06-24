2a4bb2825f011f45f07fdd7d8a689d4b5079b17b
Loz Jackson <lozjackson@talktalk.net>
2016-06-24 20:09:11 +0100

    update time-picker component

--- tests/unit/components/tt-time-picker-test.js
+++ tests/unit/components/tt-time-picker-test.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 
+const { run } = Ember;
 moduleForComponent('tt-time-picker', 'Unit | Component | tt time picker', {
   needs: ['component:tt-time-slot'],
   unit: true
@@ -23,59 +24,66 @@ test('classNames', function(assert) {
   assert.deepEqual(component.get('classNames'), ['ember-view', 'time-picker', 'container']);
 });
 
+test('output should be date', function(assert) {
+  assert.expect(1);
+  var component = this.subject();
+  this.render();
+  assert.equal(component.get('output'), 'date');
+});
+
 test('times', function(assert) {
   assert.expect(1);
   var component = this.subject();
   this.render();
   assert.deepEqual(component.get('times'), [
-    { hours: 0, mins: 0 },
-    { hours: 0, mins: 30 },
-    { hours: 1, mins: 0 },
-    { hours: 1, mins: 30 },
-    { hours: 2, mins: 0 },
-    { hours: 2, mins: 30 },
-    { hours: 3, mins: 0 },
-    { hours: 3, mins: 30 },
-    { hours: 4, mins: 0 },
-    { hours: 4, mins: 30 },
-    { hours: 5, mins: 0 },
-    { hours: 5, mins: 30 },
-    { hours: 6, mins: 0 },
-    { hours: 6, mins: 30 },
-    { hours: 7, mins: 0 },
-    { hours: 7, mins: 30 },
-    { hours: 8, mins: 0 },
-    { hours: 8, mins: 30 },
-    { hours: 9, mins: 0 },
-    { hours: 9, mins: 30 },
-    { hours: 10, mins: 0 },
-    { hours: 10, mins: 30 },
-    { hours: 11, mins: 0 },
-    { hours: 11, mins: 30 },
-    { hours: 12, mins: 0 },
-    { hours: 12, mins: 30 },
-    { hours: 13, mins: 0 },
-    { hours: 13, mins: 30 },
-    { hours: 14, mins: 0 },
-    { hours: 14, mins: 30 },
-    { hours: 15, mins: 0 },
-    { hours: 15, mins: 30 },
-    { hours: 16, mins: 0 },
-    { hours: 16, mins: 30 },
-    { hours: 17, mins: 0 },
-    { hours: 17, mins: 30 },
-    { hours: 18, mins: 0 },
-    { hours: 18, mins: 30 },
-    { hours: 19, mins: 0 },
-    { hours: 19, mins: 30 },
-    { hours: 20, mins: 0 },
-    { hours: 20, mins: 30 },
-    { hours: 21, mins: 0 },
-    { hours: 21, mins: 30 },
-    { hours: 22, mins: 0 },
-    { hours: 22, mins: 30 },
-    { hours: 23, mins: 0 },
-    { hours: 23, mins: 30 }
+    { hour: 0, minute: 0 },
+    { hour: 0, minute: 30 },
+    { hour: 1, minute: 0 },
+    { hour: 1, minute: 30 },
+    { hour: 2, minute: 0 },
+    { hour: 2, minute: 30 },
+    { hour: 3, minute: 0 },
+    { hour: 3, minute: 30 },
+    { hour: 4, minute: 0 },
+    { hour: 4, minute: 30 },
+    { hour: 5, minute: 0 },
+    { hour: 5, minute: 30 },
+    { hour: 6, minute: 0 },
+    { hour: 6, minute: 30 },
+    { hour: 7, minute: 0 },
+    { hour: 7, minute: 30 },
+    { hour: 8, minute: 0 },
+    { hour: 8, minute: 30 },
+    { hour: 9, minute: 0 },
+    { hour: 9, minute: 30 },
+    { hour: 10, minute: 0 },
+    { hour: 10, minute: 30 },
+    { hour: 11, minute: 0 },
+    { hour: 11, minute: 30 },
+    { hour: 12, minute: 0 },
+    { hour: 12, minute: 30 },
+    { hour: 13, minute: 0 },
+    { hour: 13, minute: 30 },
+    { hour: 14, minute: 0 },
+    { hour: 14, minute: 30 },
+    { hour: 15, minute: 0 },
+    { hour: 15, minute: 30 },
+    { hour: 16, minute: 0 },
+    { hour: 16, minute: 30 },
+    { hour: 17, minute: 0 },
+    { hour: 17, minute: 30 },
+    { hour: 18, minute: 0 },
+    { hour: 18, minute: 30 },
+    { hour: 19, minute: 0 },
+    { hour: 19, minute: 30 },
+    { hour: 20, minute: 0 },
+    { hour: 20, minute: 30 },
+    { hour: 21, minute: 0 },
+    { hour: 21, minute: 30 },
+    { hour: 22, minute: 0 },
+    { hour: 22, minute: 30 },
+    { hour: 23, minute: 0 },
+    { hour: 23, minute: 30 }
   ]);
 });
 
@@ -90,7 +98,7 @@ test('didInsertElement() method', function(assert) {
 test('scrollToElement() method', function(assert) {
   assert.expect(3);
   let _scheduleOnce = Ember.run.scheduleOnce;
-  var component = this.subject({
+  let component = this.subject({
     didInsertElement: () => {}
   });
 
@@ -104,4 +112,73 @@ test('scrollToElement() method', function(assert) {
   component.scrollToElement();
 
   Ember.run.scheduleOnce = _scheduleOnce;
+});
+
+test('_selectTime() method - select action', function(assert) {
+  assert.expect(1);
+  let date = new Date(0);
+  date.setHours(10);
+  date.setMinutes(30);
+  let component = this.subject({
+    select: time => assert.deepEqual(time, date)
+  });
+  this.render();
+  component._selectTime({ hour: 10, minute: 30 });
+});
+
+test('_selectTime() method - selectedTime property', function(assert) {
+  assert.expect(1);
+  let date = new Date(0);
+  date.setHours(10);
+  date.setMinutes(30);
+  let component = this.subject();
+  this.render();
+  run(() => component._selectTime({ hour: 10, minute: 30 }));
+  assert.deepEqual(component.get('selectedTime'), date);
+});
+
+test('_selectTime() method - output = date', function(assert) {
+  assert.expect(1);
+  let date = new Date(0);
+  date.setHours(10);
+  date.setMinutes(30);
+  let component = this.subject({
+    output: 'date',
+    select: time => assert.deepEqual(time, date)
+  });
+  this.render();
+  component._selectTime({ hour: 10, minute: 30 });
+});
+
+test('_selectTime() method - output = timestamp', function(assert) {
+  assert.expect(1);
+  let date = new Date(0);
+  date.setHours(10);
+  date.setMinutes(30);
+  let component = this.subject({
+    output: 'timestamp',
+    select: time => assert.deepEqual(time, date.getTime())
+  });
+  this.render();
+  component._selectTime({ hour: 10, minute: 30 });
+});
+
+test('_selectTime() method - output = object', function(assert) {
+  assert.expect(1);
+  let date = new Date(0);
+  date.setHours(10);
+  date.setMinutes(30);
+  let object = Ember.Object.create({
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    _date: date,
+    timestamp: date.getTime()
+  });
+
+  let component = this.subject({
+    output: 'object',
+    select: time => assert.deepEqual(time, object)
+  });
+  this.render();
+  component._selectTime({ hour: 10, minute: 30 });
 });
