ebf899fbd8fc06d08532e06d0c182bc223743286
Loz Jackson <lozjackson@talktalk.net>
2016-06-24 21:21:43 +0100

    update dummy app

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import ENV from '../config/environment';
+
+export default Ember.Controller.extend({
+  version: Ember.computed(function() {
+    const [version, git] = ENV.APP.version.split('+');
+    if ('development' === ENV.environment) {
+      Ember.Logger.debug(`${ENV.APP.name} ${version} ${git}`);
+    }
+    return version;
+  })
+});
