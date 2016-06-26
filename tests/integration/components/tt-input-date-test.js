cd8422024930fe38a63d5038b34b91c4c45e43db
Loz Jackson <lozjackson@talktalk.net>
2016-06-26 13:55:52 +0100

    update input-date component

--- tests/integration/components/tt-input-date-test.js
+++ tests/integration/components/tt-input-date-test.js
@@ -22,3 +22,31 @@ test('it renders', function(assert) {
 
   assert.equal(this.$().text().trim(), 'template block text');
 });
+
+test('displayFormat', function(assert) {
+  let date = new Date(1977,7,24);
+  this.set('date', date);
+
+  this.render(hbs`{{tt-input-date value=date}}`);
+
+  assert.equal(this.$('input').val(), 'Wed 24/08/1977');
+});
+
+test('change displayFormat', function(assert) {
+  let date = new Date(1977,7,24);
+  this.set('displayFormat', 'ddd DD/MM/YYYY');
+  this.set('date', date);
+
+  this.render(hbs`{{tt-input-date value=date displayFormat=displayFormat}}`);
+
+  assert.equal(this.$('input').val(), 'Wed 24/08/1977');
+
+  this.set('displayFormat', 'DD/MM/YYYY');
+  assert.equal(this.$('input').val(), '24/08/1977');
+
+  this.set('displayFormat', 'Do MMMM, YYYY');
+  assert.equal(this.$('input').val(), '24th August, 1977');
+
+  this.set('displayFormat', 'ddd Do, MMM YYYY');
+  assert.equal(this.$('input').val(), 'Wed 24th, Aug 1977');
+});
