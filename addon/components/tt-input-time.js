f4a6db7bf3e31bf1d2c17bb5ea07d3a063e47b30
Loz Jackson <lozjackson@talktalk.net>
2016-10-04 22:20:18 +0100

    update input-time component

--- addon/components/tt-input-time.js
+++ addon/components/tt-input-time.js
@@ -3,6 +3,7 @@
 */
 import Ember from 'ember';
 import layout from '../templates/components/tt-input-time';
+import format from 'ember-time-tools/utils/format';
 
 /**
   This component is a container for a TimeFieldComponent and a TimePickerComponent.
@@ -62,8 +63,7 @@ export default Ember.Component.extend({
     @private
   */
   _selectTime(date) {
-    Ember.assert(`'date' should be a javascript 'Date()' object.`, Ember.typeOf(date) === 'date');
-    this.set('value', date);
+    this.set('value', format(date, this.get('format')));
   },
 
   /**
