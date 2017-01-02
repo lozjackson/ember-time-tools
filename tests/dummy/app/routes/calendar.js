8d055f3c760fcc09aa353564183eec8c80a4fd01
Loz Jackson <lozjackson@talktalk.net>
2017-01-02 22:37:15 +0000

    update dummy app

--- tests/dummy/app/routes/calendar.js
+++ tests/dummy/app/routes/calendar.js
@@ -7,21 +7,28 @@ export default Ember.Route.extend({
       Ember.Object.create({
         id: 1,
         description: 'An event',
-        start:  new Date(2016,5,8,0,0,0).getTime(),
-        end : new Date(2016,5,11,0,0,0).getTime()
+        start:  new Date(2017,0,8,9,0,0).getTime(),
+        end : new Date(2017,0,11,9,0,0).getTime()
       }),
 
       Ember.Object.create({
         id: 2,
-        description: 'A long event',
-        start:  new Date(2016,5,18,0,0,0).getTime(),
-        end : new Date(2016,5,23,0,0,0).getTime()
+        description: 'Another event',
+        start:  new Date(2017,0,8,10,0,0).getTime(),
+        end : new Date(2017,0,11,10,0,0).getTime()
       }),
+
       Ember.Object.create({
         id: 3,
+        description: 'A long event',
+        start:  new Date(2017,0,18,0,0,0).getTime(),
+        end : new Date(2017,0,23,0,0,0).getTime()
+      }),
+      Ember.Object.create({
+        id: 4,
         description: 'Conference',
-        start:  new Date(2016,5,14,0,0,0).getTime(),
-        end : new Date(2016,5,15,0,0,0).getTime()
+        start:  new Date(2017,0,14,0,0,0).getTime(),
+        end : new Date(2017,0,15,0,0,0).getTime()
       })
     ];
     return Ember.A(events);
