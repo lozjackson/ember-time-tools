bfb1e9ea67be4b97610623db3fe670897ee01512
Loz Jackson <lozjackson@talktalk.net>
2017-01-02 22:39:44 +0000

    update dummy app

--- tests/dummy/app/routes/calendar.js
+++ tests/dummy/app/routes/calendar.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
+const MONTH = new Date().getMonth();
+
 export default Ember.Route.extend({
 
   model() {
@@ -7,28 +9,28 @@ export default Ember.Route.extend({
       Ember.Object.create({
         id: 1,
         description: 'An event',
-        start:  new Date(2017,0,8,9,0,0).getTime(),
-        end : new Date(2017,0,11,9,0,0).getTime()
+        start:  new Date(2017, MONTH ,8,9,0,0).getTime(),
+        end : new Date(2017, MONTH ,11,9,0,0).getTime()
       }),
 
       Ember.Object.create({
         id: 2,
         description: 'Another event',
-        start:  new Date(2017,0,8,10,0,0).getTime(),
-        end : new Date(2017,0,11,10,0,0).getTime()
+        start:  new Date(2017, MONTH ,8,10,0,0).getTime(),
+        end : new Date(2017, MONTH ,11,10,0,0).getTime()
       }),
 
       Ember.Object.create({
         id: 3,
         description: 'A long event',
-        start:  new Date(2017,0,18,0,0,0).getTime(),
-        end : new Date(2017,0,23,0,0,0).getTime()
+        start:  new Date(2017, MONTH ,18,0,0,0).getTime(),
+        end : new Date(2017, MONTH ,23,0,0,0).getTime()
       }),
       Ember.Object.create({
         id: 4,
         description: 'Conference',
-        start:  new Date(2017,0,14,0,0,0).getTime(),
-        end : new Date(2017,0,15,0,0,0).getTime()
+        start:  new Date(2017, MONTH ,14,0,0,0).getTime(),
+        end : new Date(2017, MONTH ,15,0,0,0).getTime()
       })
     ];
     return Ember.A(events);
