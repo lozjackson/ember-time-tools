9e4d5673537844b09e7b990ebfbd68ae27ee8b5d
Loz Jackson <lozjackson@talktalk.net>
2018-04-15 00:41:54 +0100

    Update dummy app

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -3,6 +3,9 @@ import ENV from '../config/environment';
 
 export default Ember.Controller.extend({
   version: Ember.computed(function() {
+    if (!ENV.APP || !ENV.APP.version) {
+      return;
+    }
     const [version, git] = ENV.APP.version.split('+');
     if ('development' === ENV.environment) {
       Ember.Logger.debug(`${ENV.APP.name} ${version} ${git}`);
